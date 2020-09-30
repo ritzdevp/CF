9b4b038f4921c045c7bf6328e8a788a7b57842f0
Rituraj Singh <rtjsingh30@gmail.com>
2020-10-01 02:54:23 +0530

    A566

---
+++ A566.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class A566
+{
+
+	public static void main (String[] args) throws java.lang.Exception{
+        //Scanner s = new Scanner(System.in);
+        FastReader s = new FastReader();
+        int n = s.nextInt();
+        if (n % 2 != 0){
+            System.out.println(0);
+        }
+        else{
+            System.out.println((int)Math.pow(2, n/2));
+        }
+
+	}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////-----------FAST READER------------/////////////////////////
+    static class FastReader { 
+            BufferedReader br; 
+            StringTokenizer st; 
+    
+            public FastReader() 
+            { 
+                br = new BufferedReader(new
+                        InputStreamReader(System.in)); 
+            } 
+    
+            String next() 
+            { 
+                while (st == null || !st.hasMoreElements()) 
+                { 
+                    try
+                    { 
+                        st = new StringTokenizer(br.readLine()); 
+                    } 
+                    catch (IOException  e) 
+                    { 
+                        e.printStackTrace(); 
+                    } 
+                } 
+                return st.nextToken(); 
+            } 
+    
+            int nextInt() 
+            { 
+                return Integer.parseInt(next()); 
+            } 
+    
+            long nextLong() 
+            { 
+                return Long.parseLong(next()); 
+            } 
+    
+            double nextDouble() 
+            { 
+                return Double.parseDouble(next()); 
+            } 
+    
+            String nextLine() 
+            { 
+                String str = ""; 
+                try
+                { 
+                    str = br.readLine(); 
+                } 
+                catch (IOException e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+                return str; 
+            } 
+    }
+
+}
