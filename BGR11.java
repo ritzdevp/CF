12ae8931433dbeec286fbf00627d72296b7e3fe8
Rituraj Singh <rtjsingh30@gmail.com>
2020-10-14 20:52:43 +0530

    cf practice

--- BGR11.java
+++ BGR11.java
@@ -4,76 +4,80 @@ import java.io.*;
 
 public class BGR11
 {
-    public static long func(String str, int n, int k)
-        { 
-            if (k == 0){
-                long ans = 0;
-                for (int i = 0; i < n; i++){
-                    if (str.charAt(i) == 'W'){
-                        if (i - 1 >= 0 && str.charAt(i - 1) == 'W'){
-                            ans += 2;
-                        }
-                        else{
-                            ans += 1;
-                        }
+    public static long func(String str, int n, int k){ 
+            int ans = 0;
+            int l = 0;
+            int w = 0;
+            int left = -1;
+            int right = 0;
+            boolean allLost = true;
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < n; i++){
+                if (str.charAt(i) == 'L'){
+                    if (w > 0){
+                        ans += 2 * (w - 1) + 1;
                     }
+                    w = 0;
+                    l++;
                 }
+                else{
+                    if (left == -1){
+                        left = l;
+                    }
+                    else if (l > 0){
+                        arr.add(l);
+                    }
+                    l = 0;
+                    allLost = false; 
+                    w++;
+                }
+            }
+
+            if (w > 0){
+                ans += 2 * (w - 1) + 1;
+            }
+            if (l > 0){
+                right = l;
+            }
+
+            if (allLost){
+                ans = Math.min(k, n) * 2 - 1;
+                if (ans == -1) ans = 0;
                 return ans;
             }
-     
-            int max = 0; 
-            int p = 0; 
-            int left = 0;
-            int right = 0; 
-            
-            int ansL = 0, ansR = 0;
-            while (right < n) { 
-            
-                if (str.charAt(right) != 'W') p++; 
-        
-                while (p > k) { 
-                    if (str.charAt(left) != 'W') p--; 
-                    
-                    left++; 
-                } 
-                if (right - left + 1 > max){
-                    ansL = left;
-                    ansR = right;
-                    
-                    max = right - left + 1;
+
+            Collections.sort(arr);
+            for (int i = 0; i < arr.size(); i++){
+                if (k >= arr.get(i)){
+                    ans += 2 * arr.get(i) + 1;
+                    k -= arr.get(i);
                 }
-                right++; 
-            } 
-            long sum = 0;
-            for (int i = 0; i < str.length(); i++){
-                if (str.charAt(i) == 'W'){
-                    if (i >= ansL && i <= ansR){
-                        continue;
-                    }
-                    else{
-                        if (i - 1 >= 0){
-                            if (str.charAt(i - 1) == 'W' || (i - 1 == ansR)){
-                                sum += 2;
-                            }
-                            else{
-                                sum += 1;
-                            }
-                        }
-                        else{
-                            sum += 1;
-                        }
-                    }
-                }   
+                else{
+                    ans += 2 * k;
+                    k -= k;
+                    break;
+                }
             }
-            
-            // if (ansL - 1 >= 0 && str.charAt(ansL - 1) == 'W'){
-            //     sum  += (2 * max);
-            // }
-            // else{
-                sum += 1 + (2 * (max - 1));
-            // }
-            
-            return sum;
+            if (left == -1) left = 0;
+
+            if (k != 0){
+                if (k >= right){
+                    ans += 2 * right;
+                }
+                else{
+                    ans += 2 * k;
+                }
+                k -= Math.min(k, right);
+
+                if (k >= left){
+                    ans += 2 * left;
+                }
+                else if (k != 0){
+                    ans += 2 * k;
+                }
+            }
+            return ans;
     }
 
 	public static void main (String[] args) throws java.lang.Exception{
